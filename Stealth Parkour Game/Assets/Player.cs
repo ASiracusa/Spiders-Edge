5328c450b4c366006a346f098e54ac74bf7f10fb
Rice-Puddn <TheRealRicePuddn@gmail.com>
2019-10-11 16:11:47 -0400

    our guy

---
+++ Stealth Parkour Game/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float mouseSensitivity;
+    private Rigidbody body;
+    public GameObject cam;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseSensitivity= 1;
+        body = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0));
+        cam.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"),0,0));
+        if (Input.GetKey("w"))
+        {
+            body.AddForce((Vector3.forward)*3);
+        }
+
+
+
+    }
+}
