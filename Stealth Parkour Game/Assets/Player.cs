dff604bbe92d5c9c953cc2ce7c5408714f1f40f3
Rice-Puddn <TheRealRicePuddn@gmail.com>
2019-10-21 09:26:33 -0400

    boing
    Our man can jump now, and the ability to run and jump is dependent upon being grounded

--- Stealth Parkour Game/Assets/Player.cs
+++ Stealth Parkour Game/Assets/Player.cs
@@ -10,7 +10,11 @@ public class Player : MonoBehaviour
     public GameObject cam;
     private Vector3 vecForceToAdd;
     private Vector3 temporaryVec;
-    private int numDirections;
+    private bool isGrounded;
+    private float heightOverGround;
+    private int jumpCounter;
+
+
 
 
     // Start is called before the first frame update
@@ -18,6 +22,7 @@ public class Player : MonoBehaviour
     {
         mouseSensitivity= 1;
         body = GetComponent<Rigidbody>();
+
         
     }
 
@@ -27,53 +32,84 @@ public class Player : MonoBehaviour
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0));
         cam.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"),0,0));
 
-        numDirections = 0;
+
         vecForceToAdd *= 0;
 
-        if (Input.GetKey("w"))
+        if (isGrounded)
         {
-            numDirections++;
-            vecForceToAdd = cam.transform.forward;
-              vecForceToAdd.y *=0;
-            vecForceToAdd = vecForceToAdd.normalized;
-        }
-        if (Input.GetKey("d"))
-        {
-            numDirections++;
-            temporaryVec = cam.transform.right;
-            temporaryVec.y *= 0;
-            temporaryVec = temporaryVec.normalized;
-            vecForceToAdd += temporaryVec;
-        }
-        if (Input.GetKey("s"))
-        {
-            numDirections++;
-            temporaryVec = -cam.transform.forward;
-            temporaryVec.y *= 0;
-            temporaryVec = temporaryVec.normalized;
-            vecForceToAdd += temporaryVec;
-        }
-        if (Input.GetKey("a"))
-        {
-            numDirections++;
-            temporaryVec = -cam.transform.right;
-            temporaryVec.y *= 0;
-            temporaryVec = temporaryVec.normalized;
-            vecForceToAdd += temporaryVec;
-        }
-        if (!(Input.GetKey("a") || Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("d")))
-        {
-            body.velocity.Set(body.velocity.x/10, body.velocity.y, body.velocity.z/10);
-        }
-        else {
-            if (body.velocity.magnitude < 30)
+
+            if (jumpCounter <30)
+            jumpCounter++;
+
+            if (Input.GetKey("space") && jumpCounter >= 30)
             {
+                body.AddForce(new Vector3(0, 1000, 0));
+                jumpCounter = 0;
+            }
+            if (Input.GetKey("w"))
+            {
+
+                vecForceToAdd = cam.transform.forward;
+                vecForceToAdd.y *= 0;
                 vecForceToAdd = vecForceToAdd.normalized;
-                body.AddForce((vecForceToAdd * 30));
+            }
+            if (Input.GetKey("d"))
+            {
+
+                temporaryVec = cam.transform.right;
+                temporaryVec.y *= 0;
+                temporaryVec = temporaryVec.normalized;
+                vecForceToAdd += temporaryVec;
+            }
+            if (Input.GetKey("s"))
+            {
+
+                temporaryVec = -cam.transform.forward;
+                temporaryVec.y *= 0;
+                temporaryVec = temporaryVec.normalized;
+                vecForceToAdd += temporaryVec;
+            }
+            if (Input.GetKey("a"))
+            {
+
+                temporaryVec = -cam.transform.right;
+                temporaryVec.y *= 0;
+                temporaryVec = temporaryVec.normalized;
+                vecForceToAdd += temporaryVec;
+            }
+            if (!(Input.GetKey("a") || Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("d")))
+            {
+                body.velocity.Set(body.velocity.x / 10, body.velocity.y, body.velocity.z / 10);
+            }
+            else
+            {
+                if (body.velocity.magnitude < 30)
+                {
+                    vecForceToAdd = vecForceToAdd.normalized;
+                    body.AddForce((vecForceToAdd * 30));
+                }
             }
         }
 
 
 
     }
+
+
+    // tests for grounding adapted from https://stackoverflow.com/questions/44539237/unity-checking-if-the-player-is-grounded-not-working
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8) {
+            isGrounded = true;
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isGrounded = false;
+        }
+    }
+
 }
